611f2d685fbef7d299859289896e2beacfc3953c
Alec M <alecmattu1234@gmail.com>
2026-01-10 15:01:40 -0500

    Cleanup inline styling and remove redundant element

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,55 +1,19 @@
 "use client";
 
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import MenuIcon from "@mui/icons-material/Menu";
-import Container from "@mui/material/Container";
+import { AppBar, styled, Toolbar, Typography } from "@mui/material";
 
-export const Header = () => {
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            {process.env.NEXT_PUBLIC_APP_NAME}
-          </Typography>
+const StyledTypography = styled(Typography)({
+  fontWeight: 700,
+  color: "inherit",
+  textDecoration: "none",
+});
 
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <IconButton size="large" aria-label="menu" color="inherit">
-              <MenuIcon />
-            </IconButton>
-          </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            sx={{
-              mr: 2,
-              display: { xs: "flex", md: "none" },
-              flexGrow: 1,
-              fontWeight: 700,
-              color: "inherit",
-              textDecoration: "none",
-            }}
-          >
-            {process.env.NEXT_PUBLIC_APP_NAME}
-          </Typography>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-};
+export const Header = () => (
+  <AppBar position="static">
+    <Toolbar>
+      <StyledTypography variant="h6" noWrap>
+        {process.env.NEXT_PUBLIC_APP_NAME}
+      </StyledTypography>
+    </Toolbar>
+  </AppBar>
+);
